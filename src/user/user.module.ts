737d351865adf7856f7e13114bc13d299cd27f63
Brayzonn <brayzonn@gmail.com>
2026-02-03 00:20:39 +0100

    feat(user): import SendGridModule for domain verification
    - Add SendGridModule to imports array
    - Enables UserService to inject SendGridDomainService
    - Required for domain verification functionality migrated from CustomersModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,12 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaModule } from '@/prisma/prisma.module';
 import { EmailService } from '@/email/email.service';
+import { SendGridModule } from '@/sendgrid/sendgrid.module';
+import { RedisModule } from '@/redis/redis.module';
+import { EmailModule } from '@/email/email.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, SendGridModule, RedisModule, EmailModule],
   controllers: [UserController],
   providers: [UserService, EmailService],
   exports: [UserService],
